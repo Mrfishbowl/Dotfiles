893c88a40c7b39bd23da45e3674a9bebf15fd8b7
Mr.Fishbowl <mr.fishbowl1@gmail.com>
2024-04-17 17:38:07 +0200

    added dotfiles

---
+++ config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,11 @@
+return {
+	'nvim-telescope/telescope.nvim',
+	tag = '0.1.6',
+	dependencies = { 'nvim-lua/plenary.nvim' },
+	config = function()
+		local builtin = require("telescope.builtin")
+		vim.keymap.set('n', '<C-t>', builtin.find_files, {})
+		vim.cmd('highlight TelescopeBorder guifg=#B7BDF8')
+	end
+}
+
