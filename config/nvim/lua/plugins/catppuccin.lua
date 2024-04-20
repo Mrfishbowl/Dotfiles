929f87e216e2c8eba9d722344c3b8c5f2a054281
Mr.Fishbowl <mr.fishbowl1@gmail.com>
2024-04-20 17:04:40 +0200

    updated the nvim config

--- config/nvim/lua/plugins/catppuccin.lua
+++ config/nvim/lua/plugins/catppuccin.lua
@@ -4,7 +4,13 @@ return {
 	priority = 1000,
 	config = function()
 		require("catppuccin").setup({
-			transparent_background = true
+			transparent_background = true,
+			color_overrides = {
+				all = {
+					blue = "#b7bdf8",
+					surface1 = "#b7bdf8"
+				}
+			}
 		})
 		vim.cmd ("colorscheme catppuccin-macchiato")
 	end
--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -4,5 +4,6 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim", },
 	config = function()
 		vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
+		vim.keymap.set('n', '<C-B>', ':Neotree close<CR>')
 	end
 }
