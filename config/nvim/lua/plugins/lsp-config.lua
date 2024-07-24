e8db487f46e97d23b855743b4ca23e3bd519e886
Mrfishbowl <145457065+Mrfishbowl@users.noreply.github.com>
2024-07-24 20:41:00 +0200

    2nd commit

---
+++ config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,33 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "clangd", "jedi_language_server", "biome", "cssls" } -- npm required
+			})
+		end
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.clangd.setup({ capabilities = capabilities})
+			lspconfig.jedi_language_server.setup({ capabilities = capabilities })
+			lspconfig.biome.setup({ capabilities = capabilities })
+			lspconfig.cssls.setup({ capabilities = capabilities })
+
+			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+			vim.keymap.set({ 'n' }, '<space>ca', vim.lsp.buf.code_action, {})
+		end
+	}
+}
---
+++ config/nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/tokyonight.nvim",
+  name = "tokyonight",
+  priority = 1000,
+  config = function()
+    require("tokyonight").setup({
+      style = "night",
+      transparent = true,
+    })
+    vim.cmd("colorscheme tokyonight")
+  end
+}
+
